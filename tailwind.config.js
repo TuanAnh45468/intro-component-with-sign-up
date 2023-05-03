65a4149ad9c18336b344d8314cc37537b90aef4c
tuana <tuananh45468@gmail.com>
2023-05-03 11:18:10 +0700

    complete mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,11 @@ module.exports = {
     colors: {
       red: "hsl(0, 100%, 74%)",
       green: "hsl(154, 59%, 51%)",
+      "light-green": "hsl(154, 59%, 60%)",
       blue: "hsl(248, 32%, 49%)",
       "dark-blue": "hsl(249, 10%, 26%)",
       "grayish-blue": "hsl(246, 25%, 77%)",
+      white: "#fff",
     },
     screens: {
       mobile: "375px",
